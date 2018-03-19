55a7aacf774f9aa388d9b13e6c93e697abc22c89
Albert Chang <albertchanged@gmail.com>
2018-03-18 23:01:42 -0700

    Integrate App and CreateSong (+ delete song!), connect React Router

--- client/index.js
+++ client/index.js
@@ -1,16 +1,25 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
 import SongList from './components/SongList';
+import CreateSong from './components/CreateSong';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={CreateSong} />
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };

3f9144fad340d5c1fe1ebb1f755666855d00a01a
Albert Chang <albertchanged@gmail.com>
2018-03-20 16:37:44 -0700

    Implement SongDetail and SongList

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,7 +6,7 @@ import query from '../queries/fetchSongs';
 
 class SongList extends Component {
   onSongDelete(id) {
-    this.props.mutate({ 
+    this.props.mutate({
       variables: { id }
     })
     .then(() => this.props.data.refetch());
@@ -15,7 +15,9 @@ class SongList extends Component {
     return this.props.data.songs.map(({ id, title }) => {
       return (
         <li key={id} className="collection-item">
-          {title}
+          <Link to={`/songs/${id}`}>
+            {title}
+          </Link>
           <i
             className="material-icons"
             onClick={() => this.onSongDelete(id)}
